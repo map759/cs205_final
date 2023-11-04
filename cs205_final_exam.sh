6859372bde8250aa822210d0acdd9d440c6f81d6
Monique Parrish <map759@nau.edu>
2023-11-03 19:37:37 -0700

    I modified the cs205_final_exam.sh to get the summary that was asked in for the final and I created a screenshots file but also added in the canvas just in case.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,26 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+
+#This is for the file .dat
+
+#!/bin/bash
+
+# Get the file
+filename=$1
+
+# using awk to calculate the total of pokemon and store it in a total variable
+total=$(awk 'END {print NR}' $filename)
+
+# This is to calculate the average health points ans store it in a hp_avg variable
+avg_hp=$(awk '{sum += $6} END {print sum / NR}' $filename)
+
+# This is to calculate the average attack and store it in a attack_avg variable
+attack_avg=$(awk '{sum += $7} END {print sum / NR}' $filename)
+
+# This will show the summary using echo
+echo "======= SUMMARY OF $filename ======"
+echo "# Total Pokemon: $total"
+echo "# Avg. HP: $avg_hp"
+echo "# Avg. Attack: $attack_avg"
+echo "# ======= END SUMMARY =======#"
